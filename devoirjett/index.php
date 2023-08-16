134d11ce080a9747167376358f948c41c174b1bf
dessourcesgistha <142354276+dessourcesgistha@users.noreply.github.com>
2023-08-16 14:04:10 -0700

    Add files via upload

---
+++ devoirjett/index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>profile</title>
+    <link rel="stylesheet" href=" https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"> 
+</head>
+<body>
+<section style="background-color:gray" >
+  <div class="container py-5">
+    <div class="row"style="margin-left: 140px; width:90%" >
+      <div class="col-lg-8">
+        <div class="card mb-4"> 
+          <div class="card-body text-center">
+              <img src=" mama2.jpg "alt="avatar"
+              class="rounded-circle img-fluid" style="width: 150px;"> 
+            <h5 class="">Dessources Gistha</h5>
+          <h5 class="mb-1" style="font-size: .79rem; ">informaticienne </h5>
+          </div>
+        </div>
+          </div> 
+        </div> 
+      <div class="col-lg-9" style="margin-left: 150px;">
+        <div class="card mb-4">
+          <div class="card-body">
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0 ">Nom </p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">Dessources </p>
+              </div>
+            </div>
+            <hr>
+              <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0 ">Prenom </p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0"> Gistha</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0">Email</p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">dessourcesgistha@gmail.com</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0">pays</p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0"> Haïti</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+    
+                <p class="mb-0"> telephone</p>
+              </div>
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">(509) 4927-6705</p>
+              </div>
+            </div>
+            <hr>
+             <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0">occupation</p>
+              </div>
+
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">informaticienne</p>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-sm-3">
+                <p class="mb-0">Address</p>
+              </div>
+
+              <div class="col-sm-9">
+                <p class="text-muted mb-0">Caracolie 2</p>
+              </div>
+            </div>
+          </div>
+         </div>
+     
+          <div class="col-md-20">
+            <div class="card mb-4 mb-md-0">
+              <div class="card-body">
+                <p class="mb-4"><span class="text-primary font-italic me-1">Compétence </span>   professionnel 
+                </p>
+                <p class="mb-1" style="font-size: .77rem;">php</p>
+                <div  class="progress rounded"style="height: 5px;">
+                  <div class="progress-bar" role="progressbar" style="width: 80%" aria-valuenow="80"
+                    aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <p class="mt-4mb-1" style="font-size: .77rem;">Html</p>
+                <div class="progress rounded" style="height: 5px;">
+                  <div class="progress-bar" role="progressbar" style="width: 80%" aria-valuenow="80"
+                    aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <p class="mt-4 mb-1" style="font-size: .77rem;">css</p>
+                <div class="progress rounded" style="height: 5px;">
+                  <div class="progress-bar" role="progressbar" style="width: 72%" aria-valuenow="72"
+                    aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <p class="mt-4 mb-1" style="font-size: .77rem;">SQL </p>
+                <div class="progress rounded" style="height: 5px;">
+                  <div class="progress-bar" role="progressbar" style="width: 89%" aria-valuenow="89"
+                    aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <p class="mt-4 mb-1" style="font-size: .77rem;">Java</p>
+                <div class="progress rounded" style="height: 5px;">
+                  <div class="progress-bar" role="progressbar" style="width: 55%" aria-valuenow="55"
+                    aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+              </div>
+            </div> 
+            </div>
+            <br>
+            <div class="row">
+          <div class="col-md-30" >
+            <div class="card mb-4 mb-md-0">
+              <div class="card-body">
+                <h2 class="mb-4"><span class="text-primary font-italic me-1" ></span> A propros</h2>
+                <h5 class="mb-1" style="font-size: .79rem; ">Je suis Dessources Gistha étudiante en Génie informatique du niveau trois à l’université de la rainaissance d’haïti. Je travail sur PHP, html, css, java,le cours Principe d’investigation. J’apprendre   aussi le langage SQL.
+                    Maintenant je suis à la recherche d’un entreprise dans le but d’enrichir plus l’expérience en tant que stagiaire. 
+                 </p>
+                </h5>
+                </div> 
+              </div>
+            </div>
+           </div>
+           <br>
+        </div>
+     </section> 
+    </body>
+   </html>
